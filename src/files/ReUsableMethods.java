f6e36ecfc025605281f7ff822a0a957446dbc7e3
manali <manali.kulkarni@cogniwize.com>
2023-01-05 10:41:36 +0530

    American guy commit

--- src/files/ReUsableMethods.java
+++ src/files/ReUsableMethods.java
@@ -8,6 +8,9 @@ public class ReUsableMethods {
 		JsonPath js1 = new JsonPath(response);
 		
 		System.out.println("Jira1");
+		System.out.println("Jira2");
+		System.out.println("Jira3");
+		
 		return js1;
 	}
 
